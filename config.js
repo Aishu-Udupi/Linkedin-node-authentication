b0a1561d7265cf6f4a3884e9acb8ccaad193230a
Aishu-Udupi <92358107+Aishu-Udupi@users.noreply.github.com>
2022-01-12 09:47:22 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    'linkedinAuth': {
+      'clientID': '86yubwdpnz4b73', // your App ID
+      'clientSecret': '7WZZwGA9AsArJqRJ', // your App Secret
+      'callbackURL': 'http://127.0.0.1:3000/auth/linkedin/callback'
+      
+      
+    }
+  }
